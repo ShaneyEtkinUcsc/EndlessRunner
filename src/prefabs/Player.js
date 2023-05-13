00613a1875cda1d7829bd7cb44e13b90ea8c5c6d
Shaney Etkin <setkin@ucsc.edu>
2023-05-13 02:17:20 -0700

    edit player

--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -1,15 +1,10 @@
-//rocket prefab
-class Rocket extends Phaser.GameObjects.Sprite {
+class Player extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
 
         //add object to existing scene
         scene.add.existing(this);
-        this.isFiring = false;
-        this.moveSpeed = 2;
-
-        //add rocket sound effect
-        this.sfxRocket = scene.sound.add('sfx_rocket');
+        //this.moveSpeed = 2;
     }
 
     update() {
