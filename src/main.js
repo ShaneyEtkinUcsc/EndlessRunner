294ccbccfb5e9e17b17a3022bf01a8cb6d00273f
Shaney Etkin <setkin@ucsc.edu>
2023-05-14 03:03:13 -0700

    main comment

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
+/*
+Name: Shaney Etkin
+Game title: Dart Throw
+Approx. hrs: ~28 :(
+Creative tilt justification:
+ - I'm not sure if I did anything necessarily new technically,
+ but I tried to make it so that the platforms and balls would
+ speed up as time went on.
+ - I really like my pixelart for this game. I experimented with 
+ new blending techniques on the background and the platform, and 
+ i created multiple animations for the ball, although i didn't end 
+ up using the second one.
+*/
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
