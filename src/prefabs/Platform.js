a4c8e4daf21e9a8ffbb93452235e81057c3d9c3d
Shaney Etkin <setkin@ucsc.edu>
2023-05-14 23:00:07 -0700

    error check

--- src/prefabs/Platform.js
+++ src/prefabs/Platform.js
@@ -1,7 +1,8 @@
 //platform prefab
 class Platform extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, velocity) {
-        super(scene, game.config.width, Phaser.Math.Between(0, this.game.config.height), 'platform'); 
+        //super(scene, game.config.width, Phaser.Math.Between(0, this.game.config.height), 'platform'); 
+        super(scene, game.config.width + 16, Phaser.Math.Between(64, game.config.height - 64), 'platform'); 
         
         this.parentScene = scene;  
 
